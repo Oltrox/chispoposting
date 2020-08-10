efc0aa36357c55e07a6f327cf6c5faad89f91ae3
Daniel <daniel-moran@hotmail.com>
2020-08-10 18:14:05 -0400

    Correccion de la eliminacion

--- chispoposting/src/app/app.module.ts
+++ chispoposting/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './shared/routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { AutenticacionComponent } from './components/autenticacion/autenticacion.component';
 import { LoginComponent } from './components/autenticacion/login/login.component';
-import { PruebaComponent } from './components/inicio/prueba/prueba.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AutenticacionComponent,
-    LoginComponent,
-    PruebaComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- chispoposting/src/app/shared/routing/app-routing.module.ts
+++ chispoposting/src/app/shared/routing/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AutenticacionComponent } from '../../components/autenticacion/autenticacion.component';
-import { PruebaComponent } from 'src/app/components/inicio/prueba/prueba.component';
 
 const routes: Routes = [
   {
     path: 'auth', component: AutenticacionComponent
-  },
-  {
-    path: 'test', component: PruebaComponent
   }
 ];
 
